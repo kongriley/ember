dde45242e58157573c0af6452c5cd425dd048949
BuildTools <oliver.ni@gmail.com>
2016-04-27 21:07:59 -0700

    Committed

--- js/creative.js
+++ js/creative.js
@@ -9,7 +9,7 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+        var $anchor = this;
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
